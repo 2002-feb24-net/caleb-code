199b517dac788f097d1188a9c6a27c4fab15ada4
Caleb Crownover <cj.crown@hotmail.com>
2020-02-27 16:02:29 -0600

    adding week 1 exercises after gitignore added last commit

---
+++ day4exercises/day4problem1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+//given a number of seconds, print how many years old that is for each planet
+namespace day4problem1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double secInput = GetSeconds();
+            CalculateYears(secInput);
+        }
+
+        static double GetSeconds()
+        {
+            Console.WriteLine("How many seconds should be measured?");
+            double secInput = Convert.ToDouble(Console.ReadLine());
+            return secInput;
+        }
+
+        static double CalculateYears(double secs)
+        {
+            double days = secs / 60 / 24;
+            
+            double earthYears = days / 365.26;
+            double mercuryYears = days / 87.97;
+            double venusYears = days / 224.7;
+            double marsYears = days / 686.98;
+            double jupiterYears = days / 4332.82;
+            double saturnYears = days / 10755.7;
+            double uranusYears = days / 30687.15;
+            double neptuneYears = days / 60190.03;
+            double plutoYears = days / 90533;
+
+            double[] arr = {earthYears, mercuryYears, venusYears, marsYears, jupiterYears, saturnYears, uranusYears, neptuneYears, plutoYears};
+            string[] planetName = {"Earth", "Mercury", "Venus", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune", "Pluto"};
+            for(int i = 0; i < 9; i++)
+            {
+                Console.WriteLine("That many seconds is equal to " + arr[i] + " " + planetName[i] + " years.");
+            }
+            return arr[0];
+        }
+    }
+}
---
+++ day4exercises/day4problem2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+//given a string and a number, print all the substrings of that length, ex: if string abcd and 2, should print ab, bc, cd
+namespace day4problem2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputString = GetString();
+            int inputNumber = GetNum();
+            SubstringDisplay(inputString, inputNumber);
+        }
+
+        static string GetString()
+        {
+            Console.WriteLine("Please enter a string.");
+            return Console.ReadLine();
+        }
+
+        static int GetNum()
+        {
+            Console.WriteLine("Please enter a number.");
+            return int.Parse(Console.ReadLine());
+        }
+
+        static void SubstringDisplay(string inputStr, int inputNum)
+        {
+            char[] charArr = inputStr.ToCharArray();
+            for(int i = 0; i < (inputStr.Length - inputNum + 1); i++)
+            {
+                string retString = inputStr.Substring(i, inputNum);
+                Console.WriteLine(retString);
+            }
+        }
+    }
+}
