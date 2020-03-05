d9f58297bdf142a4247d6fae8af8c7d67d1734ac
Caleb Crownover <cj.crown@hotmail.com>
2020-03-04 23:16:06 -0600

    commit of wk2day3 bubblesort

---
+++ week2exercises/BubbleSort/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace BubbleSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int[] arr = { 38, 80, 65, 13, 33, 28, 43, 7 };
+            int[] arr = { 3, 2, 1, 4, 6 };
+
+            //instantiate temporary variable for handoff
+            int temp = 0;
+
+            //moves through array length
+            for (int z = 0; z < arr.Length; z++) {
+                for (int x = 0; x < arr.Length - 1; x++) {
+                    //evaluates if int1 is less than int2
+                    if (arr[x] > arr[x + 1]) {
+                        //places int2 into temp variable
+                        temp = arr[x + 1];
+                        //places int1 into int2
+                        arr[x + 1] = arr[x];
+                        //places temp variable(int2) into int1
+                        arr[x] = temp;
+                    }
+                }
+            }
+
+            //print array
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+            //keeps program from ending b/f user confirms exit
+            Console.WriteLine("\nPress any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ week2exercises/Matrices/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Matrices
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //randomizer b/t 1 & 5
+            Random rand = new Random();
+            int rnd = rand.Next(4);
+
+            //initialize array with random number of arrays
+            int[][] jaggedArray = new int[rnd][];  //Cannot implicitly convert type 'System.Random' to 'int'
+
+            //fill out jagged array
+            for(int i = 0; i < jaggedArray.Length; i++)
+            {
+                //creates each level of the jagged array w/ random length
+                jaggedArray[i] = new int[rnd];  //Cannot implicitly convert type 'System.Random' to 'int'
+                
+                //nested for loop
+                for (int x = 0; x < jaggedArray[i].Length; x++)
+                {
+                    //to create randomized numbers for each element in each array
+                    jaggedArray[i][x] = rnd;  //Cannot implicitly convert type 'System.Random' to 'int'
+                }
+            }
+
+            //prints each array
+            for (int i = 0; i < jaggedArray.Length; i++)
+            {
+                System.Console.Write("Element({0}): ", i);
+
+                for (int j = 0; j < jaggedArray[i].Length; j++)
+                {
+                    System.Console.Write("{0}{1}", jaggedArray[i][j], j == (jaggedArray[i].Length - 1) ? "" : " ");
+                }
+                System.Console.WriteLine();            
+            }
+            // Keep the console window open in debug mode.
+            System.Console.WriteLine("Press any key to exit.");
+            System.Console.ReadKey();
+        }
+    }
+}
